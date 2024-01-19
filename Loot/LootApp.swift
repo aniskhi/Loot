def97c1f092e207cc74d0136278fa37ac552de6f
aniskhi <Anis.Khiari@etu.univ-smb.fr>
2024-01-19 10:32:46 +0100

    Initial Commit

---
+++ Loot/LootApp.swift
@@ -0,0 +1,17 @@
+//
+//  LootApp.swift
+//  Loot
+//
+//  Created by Anis KHIARI on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct LootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

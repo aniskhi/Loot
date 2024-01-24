337cad8e7294965c26547074964475809eb82842
aniskhi <Anis.Khiari@etu.univ-smb.fr>
2024-01-24 10:42:22 +0100

    Version sans les charts

---
+++ Loot/AddItemView.swift
@@ -0,0 +1,119 @@
+//
+//  AddItemView.swift
+//  Loot
+//
+//  Created by Anis KHIARI on 1/19/24.
+
+import SwiftUI
+
+struct AddItemView: View {
+    @EnvironmentObject var inventory: Inventory
+    @Environment(\.dismiss) private var dismiss
+    @State var valid : Bool = false
+    @State var name = ""
+    @State var rarity = Rarity.common
+    @State var game = Game.emptyGame
+    @State var quantity: Int = 1
+    @State var typeItem: ItemType = ItemType.bow
+    @State var attackItem: Bool = false
+    @State var attackStrength: Int = 1
+    var body: some View {
+        Form {
+            Section{
+                TextField("Nom de l'objet", text: $name)
+                Picker("Rarete", selection: $rarity){
+                    ForEach(Rarity.allCases, id: \.self){ rarity in
+                        Text(String(describing: rarity).capitalized)
+                    }
+                }
+            }
+            Section {
+                Picker("Jeu", selection: $game){
+                    Text(String(describing: "Non spécifié").capitalized)
+                    ForEach(availableGames, id: \.self){game in
+                        Text(String(describing: game.name).capitalized)
+                    }
+                    
+                }
+                Stepper("Combien : \(quantity)", value: $quantity, in: 1...50)
+            }
+            Section {
+                HStack(){
+                    Text("Type")
+                    Spacer()
+                    Text("\(typeItem.rawValue)")
+                }
+                Picker("Type", selection: $typeItem){
+                    ForEach(ItemType.allCases, id: \.self) { theItem in
+                        Text(String(describing: theItem.rawValue).capitalized)
+                    }
+                }.pickerStyle(.palette)
+                
+                
+            }
+            Section{
+                Toggle(isOn: $attackItem){
+                    Text("Item d'attaque ?")
+                }
+                if attackItem {
+                    Stepper("Force d'attaque : \(attackStrength)", value: $attackStrength, in: 1...50)
+                }
+                
+                
+            }
+            Section{
+                Button(action: {
+
+                    if(name.isEmpty){
+                        valid = true
+                    }
+                    else {
+                        inventory.addItem(withItem: LootItem(id: UUID.init() ,quantity: quantity, name: name, type: typeItem, rarity: rarity, game: game))
+                        dismiss()
+                    }
+
+                }, label: {
+                    Text("Ajouter")
+                })
+                .alert(
+                    isPresented: $valid
+                ){
+                    Alert(
+                        title: Text("Nom incorrecte "),
+                        message: Text("Le nom doit faire au minimum 3 caractères")
+                    )
+                }
+            }
+        }
+    }
+}
+enum Rarity: String, CaseIterable {
+    case common = "common"
+    case uncommon = "uncommon"
+    case rare = "rare"
+    case epic = "epic"
+    case legendary = "legendary"
+    case uniq = "uniq"
+
+    var color: Color {
+        switch self {
+        case .common:
+            return Color.gray
+        case .uncommon:
+            return Color.green
+        case .rare:
+            return Color.blue
+        case .epic:
+            return Color.purple
+        case .legendary:
+            return Color.orange
+        case .uniq:
+            return Color.red
+        }
+    }
+    
+}
+
+#Preview {
+    AddItemView()
+}
---
+++ Loot/LootItem.swift
@@ -0,0 +1,92 @@
+//
+//  LootItem.swift
+//  Loot
+//
+//  Created by Anis KHIARI on 1/19/24.
+//
+import Foundation
+import SwiftUI
+
+struct LootItem: Hashable, Identifiable{
+    var id: UUID
+    
+    var quantity: Int = 1
+    var name: String = ""
+    var type: ItemType = ItemType.bow
+    var rarity: Rarity = Rarity.uniq
+    var attackStrength: Int?
+    var game: Game = Game.emptyGame
+}
+
+enum ItemType: String, CaseIterable{
+
+    case magic = "⭐️"
+    case fire = "🔥"
+    case ice = "❄️"
+    case wind = "💨"
+    case poison = "☠️"
+    case thunder = "⚡️"
+    case dagger = "🗡️"
+    case shield = "🛡️"
+    case bow = "🏹"
+    case ring = "💍"
+    case unknown = "🎲"
+}
+
+// Définition d'un tableau 'lootItems' qui contiendra des objets 'LootItem'
+let lootItems: [LootItem] = [
+    // Création d'un 'LootItem' avec des attributs spécifiques
+    LootItem(id: UUID.init(), // Identifiant unique pour l'objet de butin
+             quantity: 3, // Quantité de cet objet
+             name: "Crystal Sword", // Nom de l'objet
+             type: .magic, // Type de l'objet (magique)
+             rarity: .epic, // Rareté de l'objet (épique)
+             attackStrength: 50, // Force d'attaque de l'objet
+             game: Game(name: "Elden Ring", // Jeu associé à cet objet
+                        genre: .rpg, // Genre du jeu (RPG)
+                        coverName: "elden_ring")), // Nom de la couverture du jeu
+
+    // Création d'un autre 'LootItem' avec des attributs similaires
+    LootItem(id: UUID.init(),
+             quantity: 1,
+             name: "Flame Bow",
+             type: .fire,
+             rarity: .legendary,
+             attackStrength: 70,
+             game: Game(name: "Skyrim",
+                        genre: .rpg,
+                        coverName: "skyrim")),
+
+    // Création d'un autre 'LootItem'
+    LootItem(id: UUID.init(),
+             quantity: 5,
+             name: "Ice Dagger",
+             type: .ice,
+             rarity: .rare,
+             attackStrength: 30,
+             game: Game(name: "WoW",
+                        genre: .mmorpg,
+                        coverName: "wow")),
+
+    // Création d'un autre 'LootItem'
+    LootItem(id: UUID.init(),
+             quantity: 2,
+             name: "Wind Cloak",
+             type: .wind,
+             rarity: .uncommon,
+             attackStrength: 10,
+             game: Game(name: "Diablo IV",
+                        genre: .looter,
+                        coverName: "diablo_IV")),
+
+    // Création d'un autre 'LootItem'
+    LootItem(id: UUID.init(),
+             quantity: 4,
+             name: "Poison Ring",
+             type: .poison,
+             rarity: .uniq,
+             attackStrength: 20,
+             game: Game(name: "CS:GO",
+                        genre: .fps,
+                        coverName: nil)) // Pas de nom de couverture spécifié
+]
